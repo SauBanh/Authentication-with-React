d21e263b2e4718b6cc0a7d3e8933214e9087919c
SauBanh <saubanh05062001@gmail.com>
2023-04-18 16:34:19 +0700

    adding route protection

--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -2,5 +2,6 @@ import { redirect } from 'react-router-dom';
 
 export function action() {
     localStorage.removeItem('token');
+    // console.log(123);
     return redirect('/auth?mode=login');
 }
